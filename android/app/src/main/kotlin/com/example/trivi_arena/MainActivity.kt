e407d97a5bc51d9f73c66c0057aacedb9f659820
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2025-04-30 07:50:39 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/trivi_arena/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.trivi_arena
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/constants.dart
@@ -0,0 +1,31 @@
+const List<String> kMainCategories = [
+  'General Knowledge',
+  'Entertainment',
+  'Science',
+  'Sports',
+  'Geography',
+  'History',
+  'Arts',
+  'Animals',
+];
+
+const List<String> kEntertainmentSubCategories = [
+  "Television",
+  "Books",
+  "Film",
+  "Video Games",
+  "Board Games",
+  "Comics",
+  "Cartoon & Animations",
+];
+
+const List<String> kScienceSubCategories = [
+  "Computers",
+  "Gadgets",
+  "Nature",
+  "Mathematics",
+];
+
+enum QuestionsDifficulties { easy, medium, hard }
+
+enum QuestionTypes { multiple, boolean }
