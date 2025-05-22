0f1027e3a9349815b60d2a01034b1a7409c8b4b2
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2025-05-22 06:47:37 +0300

    add categories images and colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,8 @@
-const List<String> kMainCategories = [
+import 'package:flutter/widgets.dart';
+
+// Quizzes Categories
+const List<String> kMainCategoriesTitles = [
   'General Knowledge',
-  'Entertainment',
-  'Science',
   'Sports',
   'Geography',
   'History',
@@ -9,23 +10,32 @@ const List<String> kMainCategories = [
   'Animals',
 ];
 
-const List<String> kEntertainmentSubCategories = [
-  "Television",
-  "Books",
-  "Film",
-  "Video Games",
-  "Board Games",
-  "Comics",
-  "Cartoon & Animations",
-];
+// Quizzes Questions Properties
+enum QuizQuestionsDifficulty { easy, medium, hard }
 
-const List<String> kScienceSubCategories = [
-  "Computers",
-  "Gadgets",
-  "Nature",
-  "Mathematics",
-];
+enum QuizQuestionsType { multiple, boolean }
 
-enum QuestionsDifficulties { easy, medium, hard }
+// Main Colors
+const Color kPrimaryColor = Color(0xFF4169E1);
+const Color kSecondaryColor = Color(0xFFFFC107);
+const Color kBackgroundColor = Color.fromARGB(255, 241, 241, 241);
+const Color kTextColor = Color(0xFF333333);
 
-enum QuestionTypes { multiple, boolean }
+// Category Colors
+const Color kGeneralKnowledgeColor = Color(0xFF1AB4BC);
+const Color kEntertainmentColor = Color(0xFF9B59B6);
+const Color kScienceColor = Color(0xFF2ECC71);
+const Color kSportsColor = Color(0xFFE67E22);
+const Color kGeographyColor = Color(0xFF3498DB);
+const Color kHistoryColor = Color(0xFFC0392B);
+const Color kArtsColor = Color(0xFFE91E63);
+const Color kAnimalsColor = Color(0xFF228B22);
+const Color kDefaultColor = Color(0xFF607D8B);
+
+// Quiz Question Colors
+const Color kQuestionTimerBackgroundColor = Color(0xFF141412);
+const Color kQuestionTimerProgressIndicatorColor = kSecondaryColor;
+const Color kQuestionTimerProgressIndicatorBackgroundColor = Color(0xFF353535);
+
+const Color kCorrectQuestionColor = Color(0xFF0BE19A);
+const Color kWrongQuestionColor = Color(0xFFFE5D5D);
